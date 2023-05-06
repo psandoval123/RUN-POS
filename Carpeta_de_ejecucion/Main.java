d696487d9a9e87e24320509b81446225200aec40
juan2702-max <jbeltrano@unal.edu.co>
2023-05-05 22:27:27 -0500

    Cambie el nombre de la carpeta

--- Carpeta_de_ejecucion/Main.java
+++ Carpeta_de_ejecucion/Main.java
@@ -1,8 +1,8 @@
-package Carpeta de ejecucion;
+package Carpeta_de_ejecucion;
 
 public class Main {
     public static void main(String[] args){
-        int x = 0;
+        int x = 1;
 
         System.out.println(x);
     }
