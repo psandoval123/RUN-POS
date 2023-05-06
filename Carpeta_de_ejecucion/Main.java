5d70d57bf6d62d0edad8ff27dea336cb8216a2d5
juan2702-max <jbeltrano@unal.edu.co>
2023-05-05 22:51:03 -0500

    Agrege atributos en la clase empleado

--- Carpeta_de_ejecucion/Main.java
+++ Carpeta_de_ejecucion/Main.java
@@ -3,7 +3,7 @@ import Empleados.*;
 
 public class Main {
     public static void main(String[] args){
-        Empleado juan = new Empleado("Juan David Beltran Orjuela");
+        Empleado juan = new Empleado("Juan David Beltran Orjuela", 1068972260);
 
 
         System.out.println(juan.get_nombre());
--- Empleados/Empleado.java
+++ Empleados/Empleado.java
@@ -1,10 +1,18 @@
 package Empleados;
 
 public class Empleado {
-    private String nombre;
+    protected static final String GERENTE = "Gerente";
+    protected static final String MESERO = "Mesero";
+    protected static final String ADMINISTRADOR = "Administrador";
+    protected static final String COCINERO = "Cocinero";
+    protected String nombre;
+    protected long numero_documento;
+    protected String tipo_empleado;
+    
 
-    public Empleado(String nombre){
+    public Empleado(String nombre, long documento){
         this.nombre = nombre;
+        this.numero_documento = documento;
     }
 
     public String get_nombre(){
