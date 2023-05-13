8cfdc502f3caba60dcbd995100f3b5b12dde5dca
juan2702-max <jbeltrano@unal.edu.co>
2023-05-13 09:13:15 -0500

    Cracion de la carpeta y clase interface de ingreso

---
+++ Inter_empleado/Interface_ingreso.java
@@ -0,0 +1,13 @@
+package Inter_empleado;
+
+import javax.swing.*;
+
+public class Interface_ingreso extends JFrame{
+    
+    protected Interface_ingreso(){
+        super("Ventana de ingreso");
+        this.setSize(800,800);
+
+    }
+    
+}
